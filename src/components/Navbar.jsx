09d75b6bcf919388108dfff9d2b8ad1edcd4fb10
rudrakshtumane <rudrakshtumane100@gmail.com>
2024-07-19 15:08:09 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,10 @@ const Navbar = (props) => {
     <>
   
 
-<div className="navbar  top-0 left-0 w-full z-10 bg-opacity-200 bg-[#1F0F18] text-[#1F0F18]">
-  <div className="navbar-start">
-    
-  </div>
-  <div className="navbar-center flex justify-center">
-    <a className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#ff0084] flex items-center justify-center">{props.name}</a>
-  </div>
-  
+<div className="navbar w-full z-10 bg-opacity-200 bg-[#1F0F18] flex items-center justify-center ">
+  <div className="navbar-center ">
+    <a className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#ff0084] ">{props.name}</a>
+  </div> 
 </div>
 </>
    
